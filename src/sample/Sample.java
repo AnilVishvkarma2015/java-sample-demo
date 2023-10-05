f173924797e03f2c6a0934588360f5cc768b3f7b
AnilVishvkarma2015 <AnilVishvkarma2015@users.noreply.github.com>
2023-10-05 12:41:58 +0530

    direct second commit!

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -4,5 +4,15 @@ public class Sample {
 
 	public static void main(String ar[]) {
 		System.out.println("This is the first sample statement!");
+		cloneCommand();
+		pushCommand();
+	}
+
+	public static void cloneCommand() {
+		System.out.println("git clone repo_url is the command to clone the repository!");
+	}
+
+	public static void pushCommand() {
+		System.out.println("git push origin repo_url is the command to push the repository!");
 	}
 }
